ded90c4ef49def6df8de83529dd502be26310d2b
rameem2003 <rameem2019@gmail.com>
2022-03-14 07:54:45 +0600

    Add project files.

---
+++ method_overload_inheritance/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace method_overoading_inheritance
+{
+    class addition
+    {
+        public void add(int a, int b)
+        {
+            int add = a + b;
+            Console.WriteLine("The result of {0} + {1} is: {2}", a, b, add);
+        }
+        public void add(int a, int b, int c)
+        {
+            int add = a + b + c;
+            Console.WriteLine("The result of {0} + {1} + {2} is: {3}", a, b, c, add);
+        }
+    }
+
+
+    class app_body
+    {
+        public static void Main()
+        {
+            addition add = new addition();
+            add.add(500, 600);
+        }
+    }
+}
